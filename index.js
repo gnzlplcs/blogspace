2c2e024e114f2fd6b1324205e5a48063e8221ec6
gnzlplcs <palaciosgm@outlook.com>
2022-12-29 21:17:32 -0500

    display blogs on page

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 /**
  Challenge:
 
- GET a list of blog from the JSON Placeholder API.
+ With the 5 blog post objects, display the `title` and `body`
+properties of the first 5 posts on the browser page.
 
- BaseURL: https://apis.scrimba.com/jsonplaceholder/
- Endpoint: /posts
-
- Since there's so many posts, let's limit the array to just 5 items.
- You can use the `.slice()` array method to just grab the first 5 objects
- from the data array that comes back from the API
+ Hints:
+ * Create a `div` in the HTML file to store these items
+ * Loop over the items creating a string of HTML elements you
+   can then put into the div with `innerHTML`
  */
+
+const postsDiv = document.getElementById("posts");
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
   method: "GET",
@@ -17,5 +18,28 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
   .then((response) => response.json())
   .then((data) => {
     const arrFive = data.slice(0, 5);
-    console.log(arrFive);
+    console.log(arrFive[0].title);
+    // arrFive.map(item => {
+    //   const post = document.createElement("section");
+    //   const postTitle = document.createElement("h2");
+    //   const postBody = document.createElement("p");
+    //   postTitle.textContent = item.title;
+    //   postBody.textContent = item.body;
+    //   post.appendChild(postTitle);
+    //   post.appendChild(postBody);
+    //   postsDiv.appendChild(post);
+    // });
+
+    // scrimba's option
+    let html = ''
+    for (let post of arrFive) {
+      html += `
+        <section>
+          <h3>${post.title}</h3>
+          <p>${post.body}</p>
+          <hr>
+        </section>
+      `
+    }
+    document.getElementById("posts").innerHTML = html;
   });
